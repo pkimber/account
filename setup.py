41c31cb01fb26481c345a5b65a8c931adedd1591
Patrick Kimber <code@pkimber.net>
2016-10-16 20:50:07 +0100

    version 0.1.08

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_readme():
 setup(
     name='kb-finance',
     packages=['finance', 'finance.migrations', 'finance.tests'],
-    version='0.1.07',
+    version='0.1.08',
     description='finance',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
